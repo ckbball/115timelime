9a97c6abff6fb1110ced2455aad52484b8d293b7
Donovan Rost <drost@ucsc.edu>
2018-10-09 12:37:17 -0700

    adding firebase (needs api keys) adding vuex store adding auth, user vuex modules adding store-moderator plugin

---
+++ timelime/src/firebase/init.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase' 
+import firestore from 'firebase/firestore'
+
+  var config = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
+};
+const firebaseApp = firebase.initializeApp(config);
+firebaseApp.firestore().settings({ timestampsInSnapshots: true })
+
+export default firebaseApp.firestore()
---
+++ timelime/src/store/index.js
@@ -0,0 +1,51 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Vuex from 'vuex'
+
+import permissions from './modules/permissions'
+import auth from './modules/auth'
+import user from './modules/user'
+import locationAdmin from './modules/locationAdmin' 
+import cart from './modules/cart'
+
+import configureModerator  from './modules/store-moderator'
+
+import * as firebase from 'firebase'
+import db from '@/firebase/init'
+
+
+Vue.use(Vuex)
+
+
+export const store = new Vuex.Store({
+	plugins: [configureModerator],
+	modules: {
+		auth: auth,
+		user: user,
+		configureModerator
+
+	},
+	state: {
+
+	},
+	getters: {
+
+
+	},
+	mutations: {
+
+
+
+	},
+	actions: {
+
+	},
+
+
+
+
+
+
+
+})
+
