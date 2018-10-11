901e30e6fcb7c3c73093e6977f9b779c9bace8d5
Donovan Rost <drost@ucsc.edu>
2018-10-11 15:55:00 -0700

    add tests/shittyexample so kenji can see how vue works

--- timelime/src/router/index.js
+++ timelime/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import ShittyExample from '@/components/tests/shittyexample'
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ const router = new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+    	path: '/shittyexample',
+    	name: 'ShittyExample',
+    	component: ShittyExample
     }
   ]
 })
