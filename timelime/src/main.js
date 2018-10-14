216defe53e84daa444de402751faa321fc14399a
Donovan Rost <drost@ucsc.edu>
2018-10-14 11:26:49 -0700

    adding apollo

--- timelime/src/main.js
+++ timelime/src/main.js
@@ -4,17 +4,48 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import firebase from 'firebase'
-import SuiVue from 'semantic-ui-vue'
 
+import SuiVue from 'semantic-ui-vue'
 import 'semantic-ui-css/semantic.min.css';
 
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { setContext } from 'apollo-link-context';
+import VueApollo from 'vue-apollo'
+
+
+const httpLink = new HttpLink({
+  // You should use an absolute URL here
+  uri: 'https://api.graph.cool/simple/v1/cjn578p2i1ctr0195zwtbg1st',
+})
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  connectToDevTools: true,
+})
+// Create a new Middleware Link using setContext
+const middlewareLink = setContext(() => ({
+  headers: {
+    authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MzkyOTk2MDcsImNsaWVudElkIjoiY2puNTc4cDJpMWN0czAxOTVtMHlxMGFndiJ9.ADl1Q0hhCDIrF2GFBctAo_BFULJBqIm9QoRCLxBz_9U'
+  }
+}))
+const link = middlewareLink.concat(httpLink);
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
+
 Vue.use(SuiVue)
+Vue.use(VueApollo)
+
 Vue.config.productionTip = false
 
 firebase.auth().onAuthStateChanged(user => {
     /* eslint-disable no-new */
     new Vue({
         el: '#app',
+    	apolloProvider,
         router,
         components: { App },
         template: '<App/>'
