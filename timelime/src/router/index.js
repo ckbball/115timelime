d607d52c8957c5ac4579a42aac298863049db522
Mia Altieri <miaaltieri@eduroam-169-233-235-226.ucsc.edu>
2018-10-14 18:31:09 -0700

    oops forgot to add this file, whoops

--- timelime/src/router/index.js
+++ timelime/src/router/index.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import bar from '@/components/general/bar'
+import TopBar from '@/components/general/TopBar'
+
+import EditProfileInfo from '@/components/user_profile/EditProfileInfo'
+import FriendButton from '@/components/user_profile/FriendButton'
 import LikeButton from '@/components/user_profile/LikeButton'
+import PostLikeButton from '@/components/user_profile/PostLikeButton'
 import UserIcon from '@/components/user_profile/UserIcon'
 import WriteButton from '@/components/user_profile/WriteButton'
 import ShittyExample from '@/components/tests/shittyexample'
@@ -18,9 +24,34 @@ const router = new Router({
       component: HelloWorld
     },
     {
+      path: '/bar',
+      name: 'bar',
+      component: bar
+    },
+    {
+      path: '/TopBar',
+      name: 'TopBar',
+      component: TopBar
+    },
+    {
+      path: '/EditProfileInfo',
+      name: 'EditProfileInfo',
+      component: EditProfileInfo
+    },
+    {
+      path: '/FriendButton',
+      name: 'FriendButton',
+      component: FriendButton
+    },
+    {
       path: '/LikeButton',
       name: 'LikeButton',
       component: LikeButton
+    },
+    {
+      path: '/PostLikeButton',
+      name: 'PostLikeButton',
+      component: PostLikeButton
     },
     {
       path: '/WriteButton',
